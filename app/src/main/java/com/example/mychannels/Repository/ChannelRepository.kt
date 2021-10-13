18f7aec2645b6f7bf8ec72bb1258b75068ac281f
Meenal <mmodi1@carnival.com>
2021-10-13 11:36:14 -0500

    Made some changes in CancelRepository.kt

--- app/src/main/java/com/example/mychannels/Repository/ChannelRepository.kt
+++ app/src/main/java/com/example/mychannels/Repository/ChannelRepository.kt
@@ -4,23 +4,28 @@ import com.example.mychannels.Network.ApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
 import javax.inject.Inject
 
 
 class ChannelRepository @Inject
 constructor(private val apiService: ApiService) {
+
+    @GET
     fun getChannelData()  = flow {
         val response = apiService.getProgramChannel()
         emit(response)
     }.flowOn(Dispatchers.IO)
 
-
+    @POST
     fun getRecordingResponse() = flow {
         val response = apiService.bookRecording()
         emit(response)
     }.flowOn(Dispatchers.IO)
 
-
+    @DELETE
     fun getCancelResposnse() = flow {
         val response = apiService.cancelResponse()
         emit(response)
